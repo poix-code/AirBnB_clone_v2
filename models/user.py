7b4bdf47d8488693c73dfa52c0ee22bd63323f42
poix-code <1996@holbertonschool.com>
2020-12-18 01:22:00 +0000

    Updated

--- models/user.py
+++ models/user.py
@@ -7,10 +7,17 @@ from sqlalchemy.orm import relationship
 import models
 
 
-class User(BaseModel, Base):
-    """This class defines users"""
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+class User(BaseModel):
+    """Represents a class User
+    """
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
+#class User(BaseModel, Base):
+   # """This class defines users"""
+   # __tablename__ = 'users'
+   # email = Column(String(128), nullable=False)
+   # password = Column(String(128), nullable=False)
+   # first_name = Column(String(128))
+   # last_name = Column(String(128))
