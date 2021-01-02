fa5612939adb4d3d0777212b357b66713f6da9a8
poix-code <1996@holbertonschool.com>
2021-01-02 23:06:38 +0000

    Try

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Distributes archives on a web server"""
+
+
+from fabric.contrib import files
+from fabric.api import env, put, run
+from os import path
+
+env.hosts = ['34.74.218.65', '35.190.174.10']
+
+
+def do_deploy(archive_path):
+    """deploy"""
+    if not path.exists(archive_path):
+        return False
+
+    dataPath = '/data/web_static/releases/'
+    tmp = archive_path.split('.')[0]
+    name = tmp.split('/')[1]
+    dst = dataPath + name
+
+    try:
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(dst))
+        run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dst))
+        run('rm -f /tmp/{}.tgz'.format(name))
+        run('mv {}/web_static/* {}/'.format(dst, dst))
+        run('rm -rf {}/web_static'.format(dst))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(dst))
+        return True
+    except:
+        return False
