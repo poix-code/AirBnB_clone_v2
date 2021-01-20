43b7fb60dee78d6c86fd2dd826d8e630041e1ef8
poix-code <1996@holbertonschool.com>
2021-01-20 21:32:01 +0000

    Regexgit add 4-number_route.py git add 4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Runs a Flask web application on port 5000"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """Displays Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays C + <text> without '_'"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<txt>", strict_slashes=False)
+def python(txt="is_cool"):
+    """Displays Python + <txt>"""
+    return "Python {}".format(txt.replace('_', ' '))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Checks for 'n' as int"""
+    return "{} is a number".format(n)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
