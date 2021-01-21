e7980e7cb69df7f272c057d43732b076e8224725
poix-code <1996@holbertonschool.com>
2021-01-21 03:20:55 +0000

    Tempaltes

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,20 +1,23 @@
-<!DOCTYPE html>
-<HTML lang="en">
-    <HEAD>
-        <TITLE>HBNB</TITLE>
-    </HEAD>
-    <BODY>
-        <H1>States</H1>
-        <UL>
-            {% for state in sts.values() | sort(attribute="name") %}
-            <LI>{{ state.id }}: <B>{{ state.name }}</B>
-                <UL>
-                    {% for city in state.cities | sort(attribute="name") %}
-                    <LI>{{ city.id }}: <B>{{ city.name }}</B></LI>
-                    {% endfor %}
-                </UL>
-            </LI>
-            {% endfor %}
-        </UL>
-    </BODY>
-</HTML>
+#!/usr/bin/python3
+"""Flask web applicationfrom flask import Flask, render_template"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states_list():
+    """Display a HTML page"""
+    return render_template('8-cities_by_states.html', sts=storage.all("State"))
+
+
+@app.teardown_appcontext
+def use_teardown(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
